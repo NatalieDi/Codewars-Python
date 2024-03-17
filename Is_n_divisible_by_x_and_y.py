887ffe7f8c8e708f20037fb1a5bcae3f2c390c1d
NatalieD <qa.job.natalie.d@gmail.com>
2024-03-16 23:14:46 -0500

    Is_n_divisible_by_x_and_y

---
+++ Is_n_divisible_by_x_and_y.py
@@ -0,0 +1,17 @@
+"""https://www.codewars.com/kata/5545f109004975ea66000086/train/python
+
+Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+
+Examples:
+1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+"""
+def is_divisible(n,x,y):
+    return n % x == 0 and n % y == 0
+
+print(is_divisible(3,1,3))  #true
+print(is_divisible(12,2,6)) #true
+print(is_divisible(100,5,3)) #false
+print(is_divisible(12,7,5))  #false
