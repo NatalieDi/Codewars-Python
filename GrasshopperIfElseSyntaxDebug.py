78ea0a2f6de9494ff4ef8533e3a4348345845a2c
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-12-04 15:07:56 -0600

    Added 2 tasks

---
+++ GrasshopperIfElseSyntaxDebug.py
@@ -0,0 +1,12 @@
+"""
+https://www.codewars.com/kata/57089707fe2d01529f00024a/train/python
+"""
+
+
+def check_alive(health):
+    return health > 0
+
+
+print(check_alive(5))
+print(check_alive(0))
+print(check_alive(-5))
