0bddc91f1601b7cc5b2e811ef4296cc8af1a82eb
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-12-04 16:26:38 -0600

    Convert a string to an array

--- ConvertStringToArray.py
+++ ConvertStringToArray.py
@@ -6,7 +6,6 @@ https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/python
 def string_to_array(s):
     return s.split(" ")
 
-
 print(string_to_array('Robin Singh'))
 print(string_to_array('1 2 3'))
 print(string_to_array(''))
