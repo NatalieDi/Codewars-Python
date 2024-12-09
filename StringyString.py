6d6b3fb5417e40addef6bf8b177ac7766b37908a
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-12-08 22:21:52 -0600

    Stringy String,

---
+++ StringyString.py
@@ -0,0 +1,11 @@
+#https://www.codewars.com/kata/563b74ddd19a3ad462000054/train/python
+
+
+def stringy(size):
+    return (size * "10")[0:size]
+
+
+print(stringy(3))
+print(stringy(5))
+print(stringy(12))
+print(stringy(28))
