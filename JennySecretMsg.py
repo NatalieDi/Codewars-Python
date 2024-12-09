dca37d67e52d85e9155c28c39d0e071410363d34
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-12-08 22:11:43 -0600

    JennySecretMsg

---
+++ JennySecretMsg.py
@@ -0,0 +1,13 @@
+"""
+https://www.codewars.com/kata/55225023e1be1ec8bc000390
+"""
+
+
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return "Hello, {name}!".format(name=name)
+
+
+print(greet("Jim"))
+print(greet("Johnny"))
