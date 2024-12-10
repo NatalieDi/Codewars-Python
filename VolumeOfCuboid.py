9dd254422906ccc15ac04d971ca02d6dc36936ea
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-12-09 19:24:12 -0600

    Volume Of a Cuboid,

---
+++ VolumeOfCuboid.py
@@ -0,0 +1,9 @@
+#  https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/python
+
+
+def get_volume_of_cuboid(length, width, height):
+    return length*width*height
+
+
+print(get_volume_of_cuboid(1, 2, 2))  # 4
+print(get_volume_of_cuboid(6.3, 2, 5))  # 63.0
