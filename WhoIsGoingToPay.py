cb9ed17ce1b8e55d06bc1ebfc65eff5515e4bcb9
NatalieDiekmann <nataliia.diekmann@gmail.com>
2024-12-09 19:33:16 -0600

    Volume Of a Cuboid,Who is going to pay for the wall?

--- WhoIsGoingToPay.py
+++ WhoIsGoingToPay.py
@@ -2,9 +2,7 @@
 
 
 def who_is_paying(name):
-    if len(name)>2: return [name, name[0:2]]
-    else:
-        return [name]
+    return [name, name[0:2]] if len(name) > 2 else [name]
 
 
 print(who_is_paying("Mexico"))  # ["Mexico", "Me"]
